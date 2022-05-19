62d0c0a1d75900858567a2c08c122edaadd1316d
Oscar Monroy <oscarmonroy657@gmail.com>
2022-05-19 07:25:56 -0600

    All the modules and componentes have been created.

---
+++ src/app/heroes/heroes.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AddHeroComponent } from './pages/add-hero/add-hero.component';
+import { SearchComponent } from './pages/search/search.component';
+import { DetailComponent } from './pages/detail/detail.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ListComponent } from './pages/list/list.component';
+
+
+
+@NgModule({
+  declarations: [
+    AddHeroComponent,
+    SearchComponent,
+    DetailComponent,
+    HomeComponent,
+    ListComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class HeroesModule { }
